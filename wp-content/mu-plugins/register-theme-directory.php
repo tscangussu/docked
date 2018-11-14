23e0e22775fe5b7adece3d26ac8fadb3be6cf769
Thiago Cangussu <thiago.cangussu@gmail.com>
2018-11-14 15:27:18 -0200

    Updated wordpress image and composer file to v3

--- wp-content/mu-plugins/register-theme-directory.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Plugin Name:  Register Theme Directory
-Description:  Register default theme directory
-*/
-if ( !defined( 'WP_DEFAULT_THEME' ) ) {
-  register_theme_directory( ABSPATH . 'wp-content/themes' );
-}
